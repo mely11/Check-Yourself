e54df3c1f09ed9fa4dbe472218f1bc25d08cbb91
11! <45051238+mely11@users.noreply.github.com>
2020-03-02 14:13:00 -0600

    Add files via upload

---
+++ lib/category_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/category_icon.dart';
+import 'package:flutter_todo/category_info.dart';
+import 'package:flutter_todo/model.dart';
+
+class CategoryCard extends StatelessWidget {
+  final Category category;
+  final VoidCallback onPressed;
+
+  CategoryCard({
+    Key key,
+    @required this.category,
+    this.onPressed,
+  }) : super(key: key) {
+    assert(this.category != null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blue[50],
+        borderRadius: BorderRadius.circular(16.0),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.black54, blurRadius: 7.0, offset: Offset(0.0, 11.0))
+        ],
+      ),
+      child: InkWell(
+        child: Padding(
+          padding: const EdgeInsets.all(21.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // image
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Hero(
+                    tag: "categoryIcon${category.title}",
+                    child: CategoryIcon(
+                        icon: category.icon, color: category.color),
+                  ),
+                ),
+              ),
+              Hero(
+                  tag: "categoryInfo${category.title}",
+                  child: CategoryInfo(category: category)),
+            ],
+          ),
+        ),
+        onTap: onPressed,
+      ),
+    );
+  }
+}
